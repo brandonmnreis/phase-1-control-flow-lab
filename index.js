f677e952d6fb71f3e28ed9c86a3b497788cf8eed
Brandon Nestor Reis <brandon.nestor.reis@gmail.com>
2023-08-10 14:21:35 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+
+  let sample
+
+  if (ride <= 400) {
+    sample = 'This one is on me!';
+  } else if (ride > 400 && ride < 2000) {
+    sample = 'That will be twenty bucks.';
+  } else if (ride > 2000 && ride < 2500) {
+    sample = 'I will gladly take your thirty bucks.'
+  } else if (ride > 2500) {
+    sample = 'No can do.'
+  }
+  return sample
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generous){
+  switch(generous){
+		case 'generous' :
+			return 'Thank you so much.'
+		case 'not as generous' :
+			return 'Thank you.'
+		default : 
+			return `Bye.`
+	}
 }
